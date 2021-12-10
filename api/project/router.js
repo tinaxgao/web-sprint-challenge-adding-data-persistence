d3ed094f5517e617cf4cbd836ec70cbcb3fd1218
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-10 13:50:08 -0500

    projects get

--- api/project/router.js
+++ api/project/router.js
@@ -4,13 +4,19 @@ const Project = require("./model");
 router.get("/", (req, res, next) => {
   Project.getProjects()
     .then((r) => {
+      r.forEach((i) => {
+        if (i.project_completed === 0) {
+          i.project_completed = false;
+        } else {
+          i.project_completed = true;
+        }
+      });
+
       res.json(r);
     })
     .catch(next);
 });
 
-
-
-
+// r.project_completed === 0 r.project_completed=false
 
 module.exports = router;
--- data/migrations/20211210044855_project-mgmt.js
+++ data/migrations/20211210044855_project-mgmt.js
@@ -4,7 +4,7 @@ exports.up = async function (knex) {
       table.increments("project_id");
       table.string("project_name").notNullable();
       table.string("project_description");
-      table.boolean("project_completed");
+      table.boolean("project_completed").notNullable().defaultTo(false);
     })
     .createTable("resources", (table) => {
       table.increments("resource_id");
@@ -15,7 +15,7 @@ exports.up = async function (knex) {
       table.increments("task_id");
       table.string("task_description").notNullable();
       table.string("task_notes");
-      table.boolean("task_completed");
+      table.boolean("task_completed").notNullable().defaultTo(false);
       table
         .integer("project_id")
         .references("project_id")
