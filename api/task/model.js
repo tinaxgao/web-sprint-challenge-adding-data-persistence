b158ad79ccbb91c2da24675ef569d688dad7fb48
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-10 15:11:49 -0500

    task model

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,19 @@
 const db = require("../../data/dbConfig");
 
-async function getTasks() {
-  return db("tasks as t");
+  async function getTasks() {
+    const t = await db("tasks")
+    .join("projects", "projects.project_id", "tasks.project_id");
+  
+    const tasks = t.map((i) => {
+      if (i.task_completed === 0) {
+        i.task_completed = false;
+      } else {
+        i.task_completed = true;
+      }
+      return i;
+    });
+  
+    return tasks;
   }
   
   module.exports = { getTasks }
