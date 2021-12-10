188a9f7035b65cd1089e90a52d17e63aba7623c7
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-10 12:29:25 -0500

    migration

--- data/migrations/20211210044855_project-mgmt.js
+++ data/migrations/20211210044855_project-mgmt.js
@@ -1,8 +1,52 @@
-
-exports.up = async function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("project", (table) => {
+      table.increments("project_id");
+      table.string("project_name").notNullable();
+      table.string("project_description");
+      table.boolean("project_completed");
+    })
+    .createTable("resource", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name").notNullable().unique();
+      table.string("resource_description");
+    })
+    .createTable("task", (table) => {
+      table.increments("task_id");
+      table.string("task_description").notNullable();
+      table.string("task_notes");
+      table.boolean("task_completed");
+      table
+        .integer("project_id")
+        .references("project_id")
+        .inTable("project")
+        .unsigned()
+        .notNullable()
+        .onDelete("CASCADE");
+    })
+    .createTable("project_resources", (table) => {
+        table.increments("project_resources_id")
+        table
+        .integer("project_id")
+        .references("project_id")
+        .inTable("project")
+        .unsigned()
+        .notNullable()
+        .onDelete("CASCADE");
+        table
+        .integer("resource_id")
+        .references("resource_id")
+        .inTable("resource")
+        .unsigned()
+        .notNullable()
+        .onDelete("CASCADE");
+    });
 };
 
-exports.down = async function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("task")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("project");
 };
